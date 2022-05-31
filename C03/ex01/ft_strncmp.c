3a79b391992b74fd093286dee52f13f1810bd313
Rudi Butarbutar <89676713+rubutar@users.noreply.github.com>
2022-05-31 19:42:42 +0800

    last 31may

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -1,38 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbutarbu <rbutarbu@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/31 15:31:25 by rbutarbu          #+#    #+#             */
-/*   Updated: 2022/05/31 19:36:22 by rbutarbu         ###   ########.fr       */
+/*   Created: 2022/05/31 19:13:18 by rbutarbu          #+#    #+#             */
+/*   Updated: 2022/05/31 19:15:22 by rbutarbu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	int i = 0;
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
-	while (s1[i] == s2[i] && s1[i] != '\0')
-	{
-		i++;
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-int	main()
-{
-	char str1[] = "";
-	char str2[] = "\x80";
-	char str3[] = "hbcd";
-	char str4[] = "Bacd";
-
-	printf("%d\n", ft_strcmp(str1, str2));
-	printf("%d\n", ft_strcmp(str3, str4));
-	printf("%d\n", strcmp(str1, str2));
-	printf("%d\n", strcmp(str3, str4));
-}
